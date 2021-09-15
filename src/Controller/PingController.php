7b75980c23aa12eb588382df71994931ab190ca1
dinhphien <dinhquyphien1997@gmail.com>
2021-09-15 14:20:32 +0700

    Initial commit

---
+++ src/Controller/PingController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PingController extends AbstractController
+{
+    #[Route('/ping', name: 'ping')]
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!12345kakkak',
+            'path' => 'src/Controller/PingController.php',
+        ]);
+    }
+}
